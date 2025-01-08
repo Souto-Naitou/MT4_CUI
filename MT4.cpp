68c0a79d02838671d8ddf457632a8a8e82ccaee5
Souto-Naitou <k023g0031@g.neec.ac.jp>
2025-01-08 15:33:33 +0900

    任意軸回転の実装

--- MT4.cpp
+++ MT4.cpp
@@ -2,8 +2,22 @@
 //
 
 #include <iostream>
+#include <Matrix4x4.h>
+#include <Vector3.h>
 
 int main()
 {
+    Vector3 axis = Vector3({ 1.0f, 1.0f, 1.0f }).Normalize();
+    float angle = 0.44f;
+    Matrix4x4 rotateMatrix = Matrix4x4::RotateAxisAngleMatrix(axis, angle);
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%.2f ", rotateMatrix.m[i][j]);
+        }
+        printf("\n");
+    }
 
 }
